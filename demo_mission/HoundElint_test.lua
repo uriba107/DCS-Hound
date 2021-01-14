52e793da3480598494750e8f4028b37c18653d44
uri_ba <uri@ssm-fans.info>
2021-01-14 02:26:26 +0200

    Added demo mission

---
+++ demo_mission/HoundElint_test.lua
@@ -0,0 +1,25 @@
+do
+    
+    Elint_blue = HoundElint:create()
+    -- Elint_blue = HoundElint:create("Migariya_Elint")
+    Elint_blue:addPlatform("ELINT_C17")
+    -- Elint_blue:addPlatform("ELINT_C130")
+    -- Elint_blue:addPlatform("Kokotse_Elint")
+    -- Elint_blue:addPlatform("Khvamli_Elint")
+
+    Elint_blue:addAdminRadioMenu()
+    tts_args = {
+        freq = 251,
+    }
+    atis_args = {
+        freq = 251.500,
+    }
+    Elint_blue:configureController(tts_args)
+    Elint_blue:configureAtis(atis_args)
+
+    Elint_blue:enableController(true)
+    Elint_blue:enableATIS()
+
+    Elint_blue:platformOn()
+
+end
