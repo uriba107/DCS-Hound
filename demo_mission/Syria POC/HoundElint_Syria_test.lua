6719cdd0867fe4be1e7407756eca45c8a844ba50
uri_ba <uri@ssm-fans.info>
2021-01-16 01:35:16 +0200

    Added Syria POC

---
+++ demo_mission/Syria POC/HoundElint_Syria_test.lua
@@ -0,0 +1,29 @@
+do
+    env.info("configuring Hound")    
+    Elint_blue = HoundElint:create("Mt_Meron_ELINT")
+    Elint_blue:addPlatform("Mt_Hermon_ELINT")
+
+    Elint_blue:addPlatform("ELINT_C130_south")
+    Elint_blue:addPlatform("ELINT_C130_north")
+    -- elint:addPlatform("ELINT_3")
+    -- elint:addPlatform("HELI_ELINT")
+
+    
+    -- elint:sensorAccurecy(0.5)
+    -- Elint_blue:addAdminRadioMenu()
+    -- tts_args = {
+    --     freq = 251,
+    -- }
+    -- atis_args = {
+    --     freq = 251.500
+    -- }
+    -- Elint_blue:configureTTS(tts_args)
+    -- Elint_blue:configureAtis(atis_args)
+
+    Elint_blue:enableController(true)
+    Elint_blue:enableATIS()
+
+    Elint_blue:platformOn()
+    env.info("Hound - End of config")    
+
+end
